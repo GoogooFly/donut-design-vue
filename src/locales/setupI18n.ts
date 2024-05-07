a8b3d4685e20c81c780f6928cf7e3b90e044041e
Donut <904208936@qq.com>
2024-05-07 15:24:27 +0800

    配置国际化

---
+++ src/locales/setupI18n.ts
@@ -0,0 +1,6 @@
+import type {App} from 'vue';
+import { createI18n } from 'vue-i18n';
+
+export function setupI18n(app: App){
+
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue';
 import 'virtual:windi.css';
 import 'vfonts/Lato.css';
 import 'vfonts/FiraCode.css';
+import './design/inedx.less';
 import {setupRouter, router} from '/@/router';
 import {setupRouterGuard} from '/@/router/guard';
 import {setupStore} from '/@/store';
 import {setupGlobDirectives} from '/@/directives';
 import {registerGlobComp} from '/@/components/registerGlobComp';
+import {setupI18n} from '/@/locales/setupI18n';
 
 function bootstrap():void {
     const app = createApp(App);
@@ -22,6 +24,8 @@ function bootstrap():void {
     setupGlobDirectives(app);
     // 注册全局组件
     registerGlobComp(app);
+    // 配置国际化
+    setupI18n(app);
 
     app.mount('#app');
 }
